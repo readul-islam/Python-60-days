43e7b62ccb1f50fa188b0ffad97462d8c01f8899
readul-islam <developer.readul@gmail.com>
2023-05-17 13:57:40 +0600

    py indentation

---
+++ indentation/index.py
@@ -0,0 +1,9 @@
+
+# Indentation refers to the spaces at the beginning of a code line.The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
+
+if 4 > 5 :
+    print('hello world')
+    if 5 > 2:
+     print("Five is greater than two!") 
+     if 5 > 2:
+        print("Five is greater than two!") 
