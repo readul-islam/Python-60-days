4a9312d123ad8b141b2aa91f11286861cb790101
readul-islam <developer.readul@gmail.com>
2023-05-17 14:09:54 +0600

    py comments

---
+++ comments/index.py
@@ -0,0 +1,7 @@
+# In python comments starts with a (#) and python will ignore them
+
+# In Python if you want to write multiline comment you can use the triple quote for example:
+
+"""
+Hi there , I'm learning python in my won way so don't will do better
+    """
