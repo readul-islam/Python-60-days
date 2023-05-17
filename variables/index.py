8d59e4d21d609fa2285ad08232e8bd125b6b3259
readul-islam <developer.readul@gmail.com>
2023-05-17 14:22:42 +0600

    python variable done

---
+++ variables/index.py
@@ -0,0 +1,39 @@
+#What's a Variable??
+
+"""Variable are containers for storing data values
+"""
+
+#python has no command for declaring a variable.
+
+#When i call this is variable
+"""
+A variable is created the moment you first assign a value to it.
+"""
+#Example
+
+x= 5
+y='john'
+
+print(x);
+
+#Casting
+#If you want to specify the data type of a variable, this can be done with casting.
+
+#Example
+
+c=str(3)
+d=int(3)
+f=float(3)
+
+#How to get type of the variable??
+#You can get the data type of a variable with the type() function.
+
+print(type(c), type(d), type(f))
+
+
+#Case-Sensitive
+#Variable names are case-sensitive.
+
+a = 4
+A = "Sally"
+#A will not overwrite a
